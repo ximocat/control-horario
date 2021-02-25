45dd0eaa476065fe9514a984d3c21eea6224d1d1
ximocat <ximocat@gmail.com>
2021-02-25 23:33:56 +0100

    Modified routes, Addes ControlHorario

---
+++ src/pages/ControlHorario.vue
@@ -0,0 +1,34 @@
+<template>
+    <q-page>
+        <div class="row justify-center">
+             <div class="q-pa-md">
+    <div class="q-gutter-sm">
+      <q-badge >
+        Model: {{ model }}
+      </q-badge>
+    </div>
+
+    <div class="q-gutter-md row items-start">
+      <q-date v-model="model" mask="YYYY-MM-DD HH:mm" />
+      <q-time v-model="time" format24h/>
+    </div>
+  </div>
+        </div>
+
+    </q-page>
+</template>
+<script>
+export default {
+    name: "PageIndex",
+    data() {
+        return{
+            etiqueta: '',
+            texto: '',
+            model: '',
+        };
+    },
+    methods: {
+
+    }
+}
+</script>
