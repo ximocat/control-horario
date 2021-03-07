5ebe6a42dafc1013a88a3fc487952ac6ebb2d65a
ximocat <ximocat@gmail.com>
2021-03-08 00:58:24 +0100

    Implementada funcion getDuracionJornada en clase JornadaTrabajo

--- src/clases/JornadaTrabajo.js
+++ src/clases/JornadaTrabajo.js
@@ -1,14 +1,29 @@
 //Clase que representa una jornada de trabajo
-class JornadaTrabajo{
-    
+class JornadaTrabajo {
+
     //Constructor
-    constructor(){
-        this.arrayIntervalosJornada=[];//Array que almacena los intervalos de una jornada
+    constructor() {
+        this.arrayIntervalos = [];//Array que almacena los intervalos de una jornada
     }
     //Metodo que añade un intervalo de jornada al array de intervalos que componen
     //una jornada
-    addIntervaloJornada(intervalo){
-        this.arrayIntervalosJornada.push(intervalo);
+    addIntervaloJornada(intervalo) {
+        this.arrayIntervalos.push(intervalo);
+    }
+
+    //Metodo que calcula el tiempo de una jornada y devuelve un string del tipo
+    //hh:mm:ss
+    getDuracionJornada() {
+        let duracion = "00:00:00";
+        //Recorro todo el array y sumo las duraciones
+        for (let i = 0; i < this.arrayIntervalos.length; i++) {
+            duracion = FuncionesAuxiliares.sumaDuracionesString(duracion,
+                FuncionesAuxiliares.duracionIntervaloString(
+                    this.arrayIntervalos[i].getFechaInicio(),
+                    this.arrayIntervalos[i].getFechaFin()));
+        }
+        return duracion;
+
     }
 
 }
