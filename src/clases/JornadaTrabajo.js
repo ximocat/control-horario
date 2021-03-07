0f00fc815888ae4c43659d7ba2c07a140b399df2
ximocat <ximocat@gmail.com>
2021-03-07 19:02:08 +0100

    Implementada clase JornadaTrabajo

---
+++ src/clases/JornadaTrabajo.js
@@ -0,0 +1,14 @@
+//Clase que representa una jornada de trabajo
+class JornadaTrabajo{
+    
+    //Constructor
+    constructor(){
+        this.arrayIntervalosJornada=[];//Array que almacena los intervalos de una jornada
+    }
+    //Metodo que añade un intervalo de jornada al array de intervalos que componen
+    //una jornada
+    addIntervaloJornada(intervalo){
+        this.arrayIntervalosJornada.push(intervalo);
+    }
+
+}
