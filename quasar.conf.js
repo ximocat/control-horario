0d3b5b59167a5d3114b068756bb3282d9a1266d2
ximocat <ximocat@gmail.com>
2021-03-17 11:57:10 +0100

    Eliminada importacion erronea

--- quasar.conf.js
+++ quasar.conf.js
@@ -87,7 +87,7 @@ module.exports = function (/* ctx */) {
       // (like functional components as one of the examples),
       // you can manually specify Quasar components/directives to be available everywhere:
       //
-      components: ['QDatetime'],
+      //components: [],
       // directives: [],
 
       // Quasar plugins
