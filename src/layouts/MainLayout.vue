82afef4c321c2e20bddbb7166a98bd9ea1f37e80
ximocat <ximocat@gmail.com>
2021-03-21 19:31:09 +0100

    Eliminados elementos innecesarios

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -16,9 +16,6 @@
         </q-toolbar-title>
 
       </q-toolbar>
-      <div>
-        <!-- <botones-registro></botones-registro> -->
-      </div>
     </q-header>
     
     <q-drawer v-model="leftDrawerOpen" show-if-above bordered content-class="bg-grey-1">
@@ -26,8 +23,6 @@
         <q-item-label header class="text-grey-8">Menú</q-item-label>
         
         
-        <!-- <EssentialLink v-for="link in essentialLinks" :key="link.title" v-bind="link"/> -->
-
         <!-- Introducir Jornada -->
         <q-item clickable to="IntroJornada">
           <q-item-section avatar>
@@ -96,46 +91,6 @@
 
 <script>
 import EssentialLink from 'components/EssentialLink.vue'
-
-//const linksData = [
-  /* {
-    title: 'Usuario',
-    caption: 'Gestión del usuario',
-    icon: 'manage_accounts',
-    link: ''
-  }, */
-  /* {
-    title: 'Jornada',
-    caption: 'Introducción de la jornada',
-    icon: 'input',
-    link: ''
-  },
-  {
-    title: 'Listado Jornadas',
-    caption: 'Listar y editar jornadas',
-    icon: 'edit',
-    link: ''
-  },
-  {
-    title: 'Estadísticas',
-    caption: 'Resumenes de los datos de la jornada',
-    icon: 'auto_graph',
-    link: ''
-  },
-  {
-    title: 'Importar/Exportar',
-    caption: 'Recuperar/Guardar datos de fuentes externas',
-    icon: 'import_export',
-    link: ''
-  },
-  {
-    title: 'About',
-    caption: 'Repositorio público de la aplicación',
-    icon: 'code',
-    link: 'https://github.com/ximocat/control-horario'
-  }
-]; */
-
 export default {
   name: 'MainLayout',
   //components: { EssentialLink },
