8e134e9e69d1e87d30c1c51514f07a93d2dfd69c
ximocat <ximocat@gmail.com>
2021-03-22 18:07:46 +0100

    Implementados elementos pagina Importar

--- src/pages/Importar.vue
+++ src/pages/Importar.vue
@@ -1,25 +1,52 @@
 <template>
-    <q-page>
-        
-    </q-page>
+  <q-page>
+    <q-card
+      class="q-mb-lg q-px-lg text-white bg-light-blue no-box-shadow text-center no-border-radius"
+    >
+      <q-card-section>
+        <h6>
+          Para importar/exportar a un archivo CSV que contenga las jornadas
+          pulsa en el botón correspondiente
+        </h6>
+        <div class="">
+          <q-btn @click="exportarCSV" color="primary" label="Exportar a CSV" />
+          <q-btn @click="importarCSV" color="primary" label="Importar desde CSV" />
+          <q-space></q-space>
+          <q-btn
+            @click="borrarLocalStorage"
+            color="red"
+            label="Borrar Datos (localstorage)"
+          />
+        </div>
+      </q-card-section>
+    </q-card>
+  </q-page>
 </template>
 <script>
-import BotonesRegistro from 'src/components/BotonesRegistro.vue';
+import FuncionesAuxiliares from "src/clases/FuncionesAuxiliares";
 export default {
-    name: "PageIndex",
-  
-        data() {
-            return{
-                etiqueta: '',
-                texto: '',
-                model: '',
-            };
+  name: "PageIndex",
+  data() {
+    return {
+      model: null,
+    };
+  },
+  methods: {
+    borrarLocalStorage: function () {
+      if (
+        confirm("Atención vas a borrar los datos actuales, ¿Quieres continuar?")
+      ) {
+        localStorage.clear();
+        FuncionesAuxiliares.leerLocalStorage();
+      }
     },
-    methods: {
-
-    },
-    components: {
+    exportarCSV: function(){
         
+    },
+    importarCSV: function(){
+
     }
+  },
+  components: {},
 };
 </script>
