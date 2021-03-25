44be0c0f4e7503ec92b783156de2a6ae418a5d55
ximocat <ximocat@gmail.com>
2021-03-25 11:14:09 +0100

    Generado componente

---
+++ src/components/Grafico.vue
@@ -0,0 +1,41 @@
+<template>
+  
+  <div>
+    <p>Hola</p>
+  </div>
+
+</template>
+
+<script>
+import FuncionesAuxiliares from "../clases/FuncionesAuxiliares.js";
+import IntervaloJornada from "../clases/IntervaloJornada.js";
+import JornadaTrabajo from "../clases/JornadaTrabajo.js";
+import ListaJornadas from "../clases/ListaJornadas.js";
+
+export default {
+  name: "grafico",
+  //********************
+  //Variables reactivas
+  //********************
+  data: function () {
+    return {
+           
+
+    };
+  },
+  //********************
+  //Variables calculadas
+  //********************
+  computed: {
+
+    
+  },
+
+  //********************
+  // Métodos
+  //********************
+  methods: {
+    
+  }
+};
+</script>
