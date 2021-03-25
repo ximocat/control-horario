ae79a9f20b36534cede7327d0be73b1471de57d9
ximocat <ximocat@gmail.com>
2021-03-25 11:14:44 +0100

    Añadido componente html grafico

--- src/components/Resumen.vue
+++ src/components/Resumen.vue
@@ -1,12 +1,7 @@
 <template>
   <div class="q-pa-md">
     <div class="q-gutter-y-md" style="max-width: 600px">
-      
-      <q-tabs
-        v-model="tab"
-        no-caps
-        class="bg-light-blue text-white shadow-2"
-      >
+      <q-tabs v-model="tab" no-caps class="bg-light-blue text-white shadow-2">
         <q-tab name="semanal" label="Semanal" />
         <q-tab name="mensual" label="Mensual" />
         <q-tab name="anual" label="Anual" />
@@ -14,15 +9,14 @@
       <q-card
         class="q-mb-lg q-px-lg text-white bg-light-blue no-box-shadow text-center no-border-radius"
       >
-         <q-card-section>
+        <q-card-section>
           <h6>Resumen {{ tab }}</h6>
-          <h6>Horas Realizadas: {{ horas }} </h6>
+          <h6>Horas Realizadas: {{ horas }}</h6>
         </q-card-section>
-        </q-card> 
+      </q-card>
     </div>
+    <grafico/>
   </div>
- 
-
 </template>
 
 <script>
@@ -30,6 +24,7 @@ import FuncionesAuxiliares from "../clases/FuncionesAuxiliares.js";
 import IntervaloJornada from "../clases/IntervaloJornada.js";
 import JornadaTrabajo from "../clases/JornadaTrabajo.js";
 import ListaJornadas from "../clases/ListaJornadas.js";
+import Grafico from "src/components/Grafico.vue";
 
 export default {
   name: "tabla-jornadas",
@@ -40,35 +35,35 @@ export default {
     return {
       tab: "semanal",
       jornadas: ListaJornadas.app,
-      
-      
-
     };
   },
   //********************
   //Variables calculadas
   //********************
   computed: {
-/*     horasSemanales: function(){
+    /*     horasSemanales: function(){
       return this.jornadas.calcularHorasSemana();
     },
     horasMensuales: function(){
       return this.jorndas.calcularHorasMes();
     }, */
 
-    horas: function(){
-      if (this.tab==="semanal") return this.jornadas.calcularHorasSemana();
-      if (this.tab==="mensual") return this.jornadas.calcularHorasMes();
-      if (this.tab==="anual") return this.jornadas.calcularHorasAnyo();
-    }
-    
+    horas: function () {
+      if (this.tab === "semanal") return this.jornadas.calcularHorasSemana();
+      if (this.tab === "mensual") return this.jornadas.calcularHorasMes();
+      if (this.tab === "anual") return this.jornadas.calcularHorasAnyo();
+    },
   },
 
   //********************
   // Métodos
   //********************
   methods: {
-    
-  }
+
+  },
+
+  components: {
+    grafico: Grafico,
+  },
 };
 </script>
